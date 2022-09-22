708878d3eb05a6eecd05578d101c5eb2859656cb
AntalThomas <Blake-Heward@hotmail.com>
2022-09-22 12:31:56 +0930

    updated readability

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,25 +1,22 @@
-def bubble_sort(numArray)
-    while true
-        numArray.each_with_index do |num, index|
-            # puts "num #{num}  index #{index}"
-            originalNum = 0
-            
-            puts "Comparing #{numArray[index]} and #{numArray[index + 1]}"
+def bubble_sort(num_array)
+  while true
+    num_array.each_with_index do |num, index|
+      puts "Comparing #{num_array[index]} and #{num_array[index + 1]}"
 
-            if index + 1 >= numArray.length()
-                next
-            elsif numArray[index] > numArray[index + 1]
-                originalNum = numArray[index + 1]
-                numArray[index + 1] = numArray[index]
-                numArray[index] = originalNum
-            end
-        end
-
-        if numArray == numArray.sort
-            break
-        end
+      if index + 1 >= num_array.length
+        next
+      elsif num_array[index] > num_array[index + 1]
+        original_num = num_array[index + 1]
+        num_array[index + 1] = num_array[index]
+        num_array[index] = original_num
+      end
     end
-    p numArray
+
+    if num_array == num_array.sort
+      break
+    end
+  end
+  p num_array
 end
 
-bubble_sort([4,3,78,2,0,2])
+bubble_sort([4, 3, 78, 2, 0, 2])
